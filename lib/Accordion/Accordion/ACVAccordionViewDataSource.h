dbacf9ec720fd31bf3ef2ad7603b8dd905ea6c05
Dylan Sturgeon <dylansturg@gmail.com>
2016-04-10 12:30:43 -0400

    create lib for ACVAccordionView (ACV = ACcordionView)

---
+++ lib/Accordion/Accordion/ACVAccordionViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  ACVAccordionViewDataSource.h
+//  Accordion
+//
+//  Created by Dylan Sturgeon on 4/10/16.
+//  Copyright © 2016 dylansturg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ACVAccordionViewDataSource <NSObject>
+    
+@end
---
+++ lib/Accordion/Accordion/Accordion.h
@@ -0,0 +1,19 @@
+//
+//  Accordion.h
+//  Accordion
+//
+//  Created by Dylan Sturgeon on 4/10/16.
+//  Copyright © 2016 dylansturg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Accordion.
+FOUNDATION_EXPORT double AccordionVersionNumber;
+
+//! Project version string for Accordion.
+FOUNDATION_EXPORT const unsigned char AccordionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Accordion/PublicHeader.h>
+
+
